e282af770eeeb84f102cd7c0726c45a9caa935c0
Warren McDonald <warrenjmac@mail.com>
2019-08-24 07:13:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,11 @@
 def using_include(array, element)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.sort 
 end
 
 def using_reverse(array)
